5588e9b162888ce7ce0f870bf56a15bc245d9e5b
hardik-kansal <hkhardik4321@gmail.com>
2024-01-10 20:15:56 +0530

    Added TD3DDPG , MarketPrice

--- Agent/chainENV.py
+++ Agent/chainENV.py
@@ -2,9 +2,42 @@ from web3 import Web3
 from configparser import ConfigParser
 import time
 import random
+import numpy as np
+import requests
+import apikey
+
+
 config = ConfigParser()
 config.read("config.ini")
-array=["0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2","0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48","0xdAC17F958D2ee523a2206206994597C13D831ec7","0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599","0x6B175474E89094C44Da98b954EedeAC495271d0F","0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2"]
+
+
+# Market Prices of Tokens
+headers = {
+  'Accepts': 'application/json',
+  'X-CMC_PRO_API_KEY': apikey.apiKey,
+}
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+
+
+def getMarketPrice():
+    symbol = []
+    for i in config['TOKENS']:
+        symbol.append(i.upper())
+    symbols_formatted=','.join(symbol)
+    parameters = {
+        'symbol': symbols_formatted,
+        'convert': 'USD'
+    }
+    json = requests.get(url, headers=headers, params=parameters).json()
+    json = json['data']
+    c=0
+    array=[]
+    for i in json:
+       array.append(json[symbol[c]]['quote']['USD']['price'])
+    return array
+
+
+
 
 routerAddr=config['UNISWAP']['address']
 routerABI=config['UNISWAP']['abi']
@@ -14,20 +47,19 @@ factoryAddr=config['FACTORY']['address']
 factoryABI=config['FACTORY']['abi']
 reservesAddr=config['RESERVES']['address']
 reservesABI=config['RESERVES']['abi']
-
-# weth
-token1="0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
-# usdc
-token0="0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
+recipient=config['Account']['address']
+privateKey=config['Account']['key']
 
 
-recipient="0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
-privateKey=0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
+
+
 
 w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
 swapContract=w3.eth.contract(address=routerAddr,abi=routerABI)
 factory=w3.eth.contract(address=factoryAddr,abi=factoryABI)
 
+
+# AT class init
 def get_weth(a=0.01,b=0.02):
     value=random.uniform(a,b)
     wethContract = w3.eth.contract(address=wethAddr, abi=wethABI)
@@ -40,7 +72,6 @@ def get_weth(a=0.01,b=0.02):
     tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
     tx = w3.eth.get_transaction(tx_hash)
     print(tx)
-
 def swap(token0,token1,amountIn,fee=3000):
     tx=w3.eth.contract(address=token0, abi=wethABI).functions.approve(routerAddr,amountIn).build_transaction({
         "from": recipient,
@@ -56,7 +87,8 @@ def swap(token0,token1,amountIn,fee=3000):
     })
     signed_tx = w3.eth.account.sign_transaction(swapTx, private_key=privateKey)
     tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
-    print("swap hash: ",tx_hash)
+    tx = w3.eth.get_transaction(tx_hash)
+    print(tx)
 
 def getReserves(token0,token1):
     poolAddr=factory.caller().getPool(token0, token1, 3000)
@@ -64,14 +96,81 @@ def getReserves(token0,token1):
     token1=w3.eth.contract(address=token1,abi=wethABI).caller().balanceOf(poolAddr)
     return token0,token1
 def reserves():
-    for i in range(0, 6):
-        for j in range(i + 1, 6):
+    array=[]
+    k = (config['TOKENS'].keys())
+    c = config['TOKENS']
+    count=0
+    for i in k:
+        array.append(c[i])
+        count+=1
+    for i in range(0,count):
+        for j in range(i + 1, count):
             print(getReserves(array[i], array[j]))
 
+
+def calculateProfit(gasUsed,marketPrice,tokenAmount):
+    tokenPrice=np.multiply(marketPrice,tokenAmount)
+    gasUsed=marketPrice[0]*gasUsed
+    return tokenPrice-gasUsed
+
+
+
+class chainENV:
+    def step(self,actions):
+        # reward
+        # next state
+        # done
+
+
+        pass
+    def reset(self):
+        # initail state
+        pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# swap(token1,token0,10000000000)
+# usdc=w3.eth.contract(address=token0,abi=wethABI).caller().balanceOf(recipient)
+# print(usdc)
+
+# print(w3.eth.get_block_transaction_count(w3.eth.get_block_number()))
+# print("Block no---")
+# print(w3.eth.get_block_number())
+# print("Getting weth--")
 # get_weth()
-reserves()
-# swap(token1,token0,1000)
+# print("blockno after weth--")
+# print(w3.eth.get_block_transaction_count(w3.eth.get_block_number()))
+#
+# reserves()
+# # print("Swap tx-")
+# swap(token1,token0,100000000000000)
+# print("blockno after swap--")
+# print(w3.eth.get_block_transaction_count(w3.eth.get_block_number()))
 # reserves()
 # c=w3.eth.contract(address=token0,abi=wethABI).caller().balanceOf(recipient)
-# print(c)
-
+# # print(c)
+# array=["0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2","0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48","0xdAC17F958D2ee523a2206206994597C13D831ec7","0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599","0x6B175474E89094C44Da98b954EedeAC495271d0F","0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2"]
+# weth
+# token1="0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
+# # usdc
+# token0="0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48"
